8d1c02cdba4fccbf5a34be488af9096886aa3bc8
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2021-05-29 10:52:44 +0200

    Initial project

---
+++ app/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.jdtech.jellyfin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
