e1e1da8ca685b5f12ac58492b887164425312a9d
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2021-05-30 11:52:53 +0200

    Setup simple navigation from AddServer to Login

--- app/src/main/java/dev/jdtech/jellyfin/AddServerFragment.kt
+++ app/src/main/java/dev/jdtech/jellyfin/AddServerFragment.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import dev.jdtech.jellyfin.databinding.FragmentAddServerBinding
 
 class AddServerFragment : Fragment() {
+    private var _binding: FragmentAddServerBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_server, container, false)
+        _binding = FragmentAddServerBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.buttonConnect.setOnClickListener { v: View ->
+            v.findNavController().navigate(R.id.action_addServerFragment_to_loginFragment)
+        }
+
+        return view
     }
 }
