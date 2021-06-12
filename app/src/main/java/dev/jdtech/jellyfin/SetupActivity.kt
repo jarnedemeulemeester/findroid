ab5b3d2e9820f1d5da695b614f9c69f410d08071
Jarne Demeulemeester <jarne.demeulemeester@student.howest.be>
2021-06-12 18:02:03 +0200

    Rename MainActivity to SetupActivity

--- app/src/main/java/dev/jdtech/jellyfin/SetupActivity.kt
+++ app/src/main/java/dev/jdtech/jellyfin/SetupActivity.kt
@@ -3,9 +3,9 @@ package dev.jdtech.jellyfin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_setup)
     }
 }
