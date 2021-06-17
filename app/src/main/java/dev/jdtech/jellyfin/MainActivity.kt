8ccc6a0ef7abeba44693397722daf7d21fac0bbe
Jarne Demeulemeester <jarne.demeulemeester@student.howest.be>
2021-06-17 18:53:52 +0200

    Add basic Toolbar

--- app/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
+++ app/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
@@ -3,8 +3,9 @@ package dev.jdtech.jellyfin
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dev.jdtech.jellyfin.databinding.ActivityMainBinding
 
@@ -23,7 +24,18 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
 
+        setSupportActionBar(binding.mainToolbar)
+
         val navController = navHostFragment.navController
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_media, R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
