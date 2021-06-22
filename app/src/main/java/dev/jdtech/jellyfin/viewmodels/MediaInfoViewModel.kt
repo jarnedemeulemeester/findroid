e254709f915e2837807e35045135d3e73542e530
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2021-06-22 17:30:52 +0200

    Add basic animations to navigation to MediaInfoFragment

--- app/src/main/java/dev/jdtech/jellyfin/viewmodels/MediaInfoViewModel.kt
+++ app/src/main/java/dev/jdtech/jellyfin/viewmodels/MediaInfoViewModel.kt
@@ -1,7 +1,6 @@
 package dev.jdtech.jellyfin.viewmodels
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
