4342448c1e550bbc578ce42f8c9e19d6f9629b44
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2021-07-04 12:29:17 +0200

    Add splash screen

--- app/src/main/java/dev/jdtech/jellyfin/SetupActivity.kt
+++ app/src/main/java/dev/jdtech/jellyfin/SetupActivity.kt
@@ -2,10 +2,12 @@ package dev.jdtech.jellyfin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class SetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_setup)
     }
 }
