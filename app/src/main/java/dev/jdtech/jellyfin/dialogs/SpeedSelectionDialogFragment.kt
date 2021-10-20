11ff3b4e16d57435ef806552172d5a9583955bd9
NIK679 <33275965+NIK679@users.noreply.github.com>
2021-10-20 23:46:43 +0530

    Add playback speed control to the player (#46)
    * add playback speed control to MPV player
    
    * add playback speed control to exoplayer
    
    * remove unnecessary method
    
    * remove unused import
    
    * use gauge icon from lucide
    
    * use gauge icon from lucide
    
    * fix the gauge icon

---
+++ app/src/main/java/dev/jdtech/jellyfin/dialogs/SpeedSelectionDialogFragment.kt
@@ -0,0 +1,35 @@
+package dev.jdtech.jellyfin.dialogs
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dev.jdtech.jellyfin.viewmodels.PlayerActivityViewModel
+import java.lang.IllegalStateException
+
+class SpeedSelectionDialogFragment(
+    private val viewModel: PlayerActivityViewModel
+) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val speedTexts = listOf("0.5x", "0.75x", "1x", "1.25x", "1.5x", "1.75x", "2x")
+        val speedNumbers = listOf(0.5f, 0.75f, 1f, 1.25f, 1.5f, 1.75f, 2f)
+
+
+        return activity?.let { activity ->
+            val builder = MaterialAlertDialogBuilder(activity)
+            builder.setTitle("Select playback speed")
+                .setSingleChoiceItems(
+                    speedTexts.toTypedArray(),
+                    speedNumbers.indexOf(viewModel.playbackSpeed)
+                ) { dialog, which ->
+                    viewModel.selectSpeed(
+                        speedNumbers[which]
+                    )
+                    dialog.dismiss()
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+
+
+    }
+}
