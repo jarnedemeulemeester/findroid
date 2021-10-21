89ee54249e9a695e08bcd313298951b9e33562a7
lsrom <lukas.srom@gmail.com>
2021-10-22 01:17:34 +0200

    Track selection dialog PoC

--- app/src/main/java/dev/jdtech/jellyfin/tv/TvPlayerActivity.kt
+++ app/src/main/java/dev/jdtech/jellyfin/tv/TvPlayerActivity.kt
@@ -1,12 +1,18 @@
 package dev.jdtech.jellyfin.tv
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.KeyEvent
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.ImageButton
+import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.navigation.navArgs
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jdtech.jellyfin.BasePlayerActivity
 import dev.jdtech.jellyfin.PlayerActivityArgs
@@ -39,6 +45,15 @@ internal class TvPlayerActivity : BasePlayerActivity() {
         hideSystemUI()
     }
 
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return if (!binding.playerView.isControllerVisible) {
+            binding.playerView.showController()
+            true
+        } else {
+            false
+        }
+    }
+
     private fun bind() = with(binding.playerView) {
         val videoNameTextView = findViewById<TextView>(R.id.video_name)
         viewModel.currentItemTitle.observe(this@TvPlayerActivity, { title ->
@@ -61,18 +76,78 @@ internal class TvPlayerActivity : BasePlayerActivity() {
             }
         }
 
-        findViewById<ImageButton>(R.id.btn_audio_track).apply {
-            isEnabled = false
-            imageAlpha = 75
-        }
-
-        findViewById<ImageButton>(R.id.btn_subtitle).apply {
-            isEnabled = false
-            imageAlpha = 75
-        }
-
         findViewById<View>(R.id.back_button).setOnClickListener {
             onBackPressed()
+        }
+
+        bindSubtitleControl()
+    }
+
+    private fun bindSubtitleControl() {
+        val subtitleBtn = binding.playerView.findViewById<ImageButton>(R.id.btn_subtitle)
+
+        subtitleBtn.setOnClickListener {
+            val items = viewModel.currentSubtitleTracks.map { track ->
+                Track(track.title, track.lang, track.codec, track.selected)
+            }
+            subtitleBtn.showPopupWindowAbove(items)
+        }
+    }
+
+    private fun View.showPopupWindowAbove(items: List<Track>) {
+        val popup = PopupWindow(this.context)
+        popup.contentView = LayoutInflater.from(context).inflate(R.layout.track_selector, null)
+        val recyclerView = popup.contentView.findViewById<RecyclerView>(R.id.track_selector)
+        recyclerView.adapter = TrackSelectorAdapter(items)
+
+        val startViewCoords = IntArray(2)
+        getLocationInWindow(startViewCoords)
+
+        val itemHeight = resources.getDimension(R.dimen.track_selection_item_height).toInt()
+        val totalHeight = items.size * itemHeight
+
+        popup.showAsDropDown(
+            binding.root,
+            startViewCoords[0],
+            startViewCoords[1] - totalHeight,
+            Gravity.TOP
+        )
+    }
+
+    data class Track(
+        val title: String,
+        val language: String,
+        val codec: String,
+        val selected: Boolean
+    )
+
+    class TrackSelectorAdapter(
+        private val items: List<Track>
+    ) : RecyclerView.Adapter<TrackSelectorAdapter.TrackSelectorViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackSelectorViewHolder {
+            return TrackSelectorViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: TrackSelectorViewHolder, position: Int) {
+            holder.bind(items[position])
+        }
+
+        override fun getItemCount(): Int = items.size
+
+        class TrackSelectorViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+            fun bind(item: Track) {
+                view.findViewById<TextView>(R.id.track_name).text = String.format(
+                    view.resources.getString(R.string.track_selection),
+                    item.language,
+                    item.title,
+                    item.codec
+                )
+                if (item.selected) view.requestFocus()
+            }
         }
     }
 }
