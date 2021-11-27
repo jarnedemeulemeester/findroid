68118c4d8a69d2908d300189ebe7ebce72a47c5f
Jarne Demeulemeester <jarne.demeulemeester@student.howest.be>
2021-11-27 16:02:29 +0100

    Add background to the overlays

--- app/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
+++ app/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
@@ -2,7 +2,6 @@ package dev.jdtech.jellyfin
 
 import android.content.Context
 import android.media.AudioManager
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
