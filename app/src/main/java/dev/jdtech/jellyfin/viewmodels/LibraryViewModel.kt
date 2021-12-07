87ffa72e2345d65091dce96a93fb37c40c9cf810
jarnedemeulemeester <jarnedemeulemeester@gmail.com>
2021-12-07 16:22:42 +0100

    Convert LibraryFragment to use new UiState

--- app/src/main/java/dev/jdtech/jellyfin/viewmodels/LibraryViewModel.kt
+++ app/src/main/java/dev/jdtech/jellyfin/viewmodels/LibraryViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.jdtech.jellyfin.repository.JellyfinRepository
 import dev.jdtech.jellyfin.utils.SortBy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.jellyfin.sdk.model.api.BaseItemDto
 import org.jellyfin.sdk.model.api.SortOrder
@@ -14,16 +16,20 @@ import javax.inject.Inject
 @HiltViewModel
 class LibraryViewModel
 @Inject
-constructor(private val jellyfinRepository: JellyfinRepository) : ViewModel() {
+constructor(
+    private val jellyfinRepository: JellyfinRepository
+) : ViewModel() {
+    private val uiState = MutableStateFlow<UiState>(UiState.Loading)
 
-    private val _items = MutableLiveData<List<BaseItemDto>>()
-    val items: LiveData<List<BaseItemDto>> = _items
+    sealed class UiState {
+        data class Normal(val items: List<BaseItemDto>) : UiState()
+        object Loading : UiState()
+        data class Error(val message: String?) : UiState()
+    }
 
-    private val _finishedLoading = MutableLiveData<Boolean>()
-    val finishedLoading: LiveData<Boolean> = _finishedLoading
-
-    private val _error = MutableLiveData<String>()
-    val error: LiveData<String> = _error
+    fun onUiState(scope: LifecycleCoroutineScope, collector: (UiState) -> Unit) {
+        scope.launch { uiState.collect { collector(it) } }
+    }
 
     fun loadItems(
         parentId: UUID,
@@ -31,8 +37,6 @@ constructor(private val jellyfinRepository: JellyfinRepository) : ViewModel() {
         sortBy: SortBy = SortBy.defaultValue,
         sortOrder: SortOrder = SortOrder.ASCENDING
     ) {
-        _error.value = null
-        _finishedLoading.value = false
         Timber.d("$libraryType")
         val itemType = when (libraryType) {
             "movies" -> "Movie"
@@ -40,19 +44,19 @@ constructor(private val jellyfinRepository: JellyfinRepository) : ViewModel() {
             else -> null
         }
         viewModelScope.launch {
+            uiState.emit(UiState.Loading)
             try {
-                _items.value = jellyfinRepository.getItems(
+                val items = jellyfinRepository.getItems(
                     parentId,
                     includeTypes = if (itemType != null) listOf(itemType) else null,
                     recursive = true,
                     sortBy = sortBy,
                     sortOrder = sortOrder
                 )
+                uiState.emit(UiState.Normal(items))
             } catch (e: Exception) {
-                Timber.e(e)
-                _error.value = e.toString()
+                uiState.emit(UiState.Error(e.message))
             }
-            _finishedLoading.value = true
         }
     }
 }
