eb89469ccc99afafe57279c91919d412f5033438
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2021-12-20 21:44:46 +0100

    Upgrade kotlin version to 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion = "1.6.0"
     repositories {
         google()
         mavenCentral()
@@ -10,6 +9,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
+
+        val kotlinVersion = "1.6.10"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
