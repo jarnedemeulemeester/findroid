42199ecaf50b06c5c5f089d1b2f5a16444f5c811
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-01-21 18:01:07 +0100

    Upgrade dependencies
    core-splashscreen 1.0.0-alpha02 -> 1.0.0-beta01
    appcompat 1.4.0 -> 1.4.1
    constraintlayout 2.1.2 -> 2.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
     implementation("androidx.leanback:leanback:1.2.0-alpha02")
 
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.core:core-splashscreen:1.0.0-alpha02")
-    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.core:core-splashscreen:1.0.0-beta01")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
 
@@ -75,7 +75,7 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
 
     // ConstraintLayout
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 
     // Lifecycle
     val lifecycleVersion = "2.4.0"
