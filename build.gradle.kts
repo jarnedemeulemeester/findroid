717e530b3c67bc509c27e50feae8898db164c863
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-01-30 12:04:55 +0100

    Upgrade gradle
    gradle 7.0.2 -> 7.2
    gradle plugin 7.0.4 -> 7.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.0")
 
         val kotlinVersion = "1.6.10"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
