a7e63eb1bbac012e5d67d905d42a1ae168b939bf
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-03-28 14:07:31 +0200

    Add options to disable player gestures and remember screen brightness

--- app/src/main/java/dev/jdtech/jellyfin/utils/Constants.kt
+++ app/src/main/java/dev/jdtech/jellyfin/utils/Constants.kt
@@ -1,14 +1,14 @@
 package dev.jdtech.jellyfin.utils
 
 object Constants {
-
-    //player
+    // player
     const val GESTURE_EXCLUSION_AREA_TOP = 48
     const val FULL_SWIPE_RANGE_SCREEN_RATIO = 0.66f
     const val ZOOM_SCALE_BASE = 1f
     const val ZOOM_SCALE_THRESHOLD = 0.01f
 
-    //pref
+    // pref
+    const val PREF_PLAYER_GESTURES = "pref_player_gestures"
+    const val PREF_PLAYER_BRIGHTNESS_REMEMBER = "pref_player_brightness_remember"
     const val PREF_PLAYER_BRIGHTNESS = "pref_player_brightness"
-
 }
