904637c80db291067f4a407bd9dd792890d18cbc
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-04-11 20:49:59 +0200

    Bump versionCode to 9 and versionName to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.jdtech.jellyfin"
         minSdk = 26
         targetSdk = 32
-        versionCode = 8
-        versionName = "0.3.2"
+        versionCode = 9
+        versionName = "0.4.0"
     }
 
     buildTypes {
