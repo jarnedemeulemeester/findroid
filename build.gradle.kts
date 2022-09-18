6a59473c8b424b3063244e8e71ab6ceb8e7ca0dc
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-09-18 16:02:31 +0200

    Upgrade agp to 7.3.0
    agp 7.3.0-rc01 -> 7.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0-rc01")
+        classpath("com.android.tools.build:gradle:7.3.0")
 
         val kotlinVersion = "1.7.10"
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
