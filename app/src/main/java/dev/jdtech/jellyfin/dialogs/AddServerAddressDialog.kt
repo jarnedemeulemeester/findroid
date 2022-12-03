37632e64cba1bfd0b663ea17274c41de9d35e647
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2022-12-03 20:34:36 +0100

    Fix AddServerAddressDialog crashing on tv

--- app/src/main/java/dev/jdtech/jellyfin/dialogs/AddServerAddressDialog.kt
+++ app/src/main/java/dev/jdtech/jellyfin/dialogs/AddServerAddressDialog.kt
@@ -1,8 +1,12 @@
 package dev.jdtech.jellyfin.dialogs
 
 import android.app.Dialog
+import android.app.UiModeManager
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dev.jdtech.jellyfin.R
@@ -13,9 +17,15 @@ class AddServerAddressDialog(
     private val viewModel: ServerAddressesViewModel
 ) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val uiModeManager =
+            requireContext().getSystemService(AppCompatActivity.UI_MODE_SERVICE) as UiModeManager
         val editText = EditText(this.context)
         return activity?.let { activity ->
-            val builder = MaterialAlertDialogBuilder(activity)
+            val builder = if (uiModeManager.currentModeType == Configuration.UI_MODE_TYPE_TELEVISION) {
+                AlertDialog.Builder(activity)
+            } else {
+                MaterialAlertDialogBuilder(activity)
+            }
             builder
                 .setTitle("Add server address")
                 .setView(editText)
