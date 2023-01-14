19a96448fd17a2d59aafb2d34e080341ee040753
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-01-14 20:21:02 +0100

    Set Java version to 11

--- player/video/build.gradle.kts
+++ player/video/build.gradle.kts
@@ -15,7 +15,6 @@ android {
 
     defaultConfig {
         minSdk = 27
-        targetSdk = 33
     }
 
     buildTypes {
@@ -28,8 +27,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
