3f25fd2d7da6e8ea2b01d57f1a6c74932c924950
Jcuhfehl <jesper@justgamingltd.nl>
2023-01-31 20:24:43 +0100

    Add season download season button
    Still needs some UI work to indicate something actually happening, but functional otherwise

--- app/phone/src/main/java/dev/jdtech/jellyfin/fragments/SeasonFragment.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/fragments/SeasonFragment.kt
@@ -2,8 +2,13 @@ package dev.jdtech.jellyfin.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -13,6 +18,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import dev.jdtech.jellyfin.R
 import dev.jdtech.jellyfin.adapters.EpisodeListAdapter
 import dev.jdtech.jellyfin.databinding.FragmentSeasonBinding
 import dev.jdtech.jellyfin.dialogs.ErrorDialogFragment
@@ -37,6 +43,25 @@ class SeasonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSeasonBinding.inflate(inflater, container, false)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.season_menu, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.action_download_season -> {
+                            viewModel.download()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            },
+            viewLifecycleOwner, Lifecycle.State.RESUMED
+        )
         return binding.root
     }
 
--- core/src/main/java/dev/jdtech/jellyfin/viewmodels/SeasonViewModel.kt
+++ core/src/main/java/dev/jdtech/jellyfin/viewmodels/SeasonViewModel.kt
@@ -1,10 +1,14 @@
 package dev.jdtech.jellyfin.viewmodels
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.jdtech.jellyfin.database.DownloadDatabaseDao
 import dev.jdtech.jellyfin.models.EpisodeItem
 import dev.jdtech.jellyfin.repository.JellyfinRepository
+import dev.jdtech.jellyfin.utils.requestDownload
+import kotlinx.coroutines.GlobalScope
 import java.util.UUID
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,7 +20,9 @@ import org.jellyfin.sdk.model.api.ItemFields
 class SeasonViewModel
 @Inject
 constructor(
-    private val jellyfinRepository: JellyfinRepository
+    private val application: Application,
+    private val jellyfinRepository: JellyfinRepository,
+    private val downloadDatabase: DownloadDatabaseDao
 ) : ViewModel() {
     private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
     val uiState = _uiState.asStateFlow()
@@ -35,6 +41,25 @@ constructor(
                 _uiState.emit(UiState.Normal(episodes))
             } catch (e: Exception) {
                 _uiState.emit(UiState.Error(e))
+            }
+        }
+    }
+
+    fun download() {
+        GlobalScope.launch {
+            uiState.collect { uiState ->
+                if (uiState !is UiState.Normal) return@collect
+                val episodes = uiState.episodes
+                episodes.forEach {
+                    try{
+                        requestDownload(
+                            jellyfinRepository,
+                            downloadDatabase,
+                            application,
+                            it.id
+                        )
+                    } catch (_ : Exception) { }
+                }
             }
         }
     }
