e32a390d1a5ac941c20f255a8b710ddff51469ed
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-02-14 01:53:12 +0530

    fix: Implementation of Mandatory System Gesture Insets to Avoid Conflicts with System Gestures (#286)
    * fix: gesture conflict in seek gesture
    
    * fix: gesture conflict in volume and brightness gestures
    
    * refactor: move to separate method and add implementation for previous Android versions
    
    * lint: run ktlintFormat
    
    ---------
    
    Co-authored-by: Jarne Demeulemeester <jarnedemeulemeester@gmail.com>

--- preferences/src/main/java/dev/jdtech/jellyfin/Constants.kt
+++ preferences/src/main/java/dev/jdtech/jellyfin/Constants.kt
@@ -2,7 +2,8 @@ package dev.jdtech.jellyfin
 
 object Constants {
     // player
-    const val GESTURE_EXCLUSION_AREA_TOP = 48
+    const val GESTURE_EXCLUSION_AREA_VERTICAL = 48
+    const val GESTURE_EXCLUSION_AREA_HORIZONTAL = 24
     const val FULL_SWIPE_RANGE_SCREEN_RATIO = 0.66f
     const val ZOOM_SCALE_BASE = 1f
     const val ZOOM_SCALE_THRESHOLD = 0.01f
