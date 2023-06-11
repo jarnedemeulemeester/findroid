3488538a04d5645cc27b9a7d0df952eb77274551
Cd16d <davidecavagnola@gmail.com>
2023-06-11 19:22:12 +0200

    fix: gesture not working if the player was locked and used back gesture

--- app/phone/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/PlayerActivity.kt
@@ -57,6 +57,8 @@ class PlayerActivity : BasePlayerActivity() {
         val playerControls = binding.playerView.findViewById<View>(R.id.player_controls)
         val lockedControls = binding.playerView.findViewById<View>(R.id.locked_player_view)
 
+        isControlsLocked = false
+
         configureInsets(playerControls)
         configureInsets(lockedControls)
 
@@ -75,7 +77,6 @@ class PlayerActivity : BasePlayerActivity() {
 
         binding.playerView.findViewById<View>(R.id.back_button_alt).setOnClickListener {
             finish()
-            isControlsLocked = false
         }
 
         val videoNameTextView = binding.playerView.findViewById<TextView>(R.id.video_name)
