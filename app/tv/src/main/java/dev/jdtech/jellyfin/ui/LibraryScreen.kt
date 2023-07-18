3923afa74eecaf4e2896787519e6df326b02b6e2
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-07-18 14:26:03 +0200

    refactor: preview `LibraryScreen`

--- app/tv/src/main/java/dev/jdtech/jellyfin/ui/LibraryScreen.kt
+++ app/tv/src/main/java/dev/jdtech/jellyfin/ui/LibraryScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.tv.foundation.lazy.grid.TvGridCells
 import androidx.tv.foundation.lazy.grid.TvGridItemSpan
@@ -19,14 +21,19 @@ import androidx.tv.foundation.lazy.grid.TvLazyVerticalGrid
 import androidx.tv.material3.CompactCard
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Surface
 import androidx.tv.material3.Text
 import com.ramcosta.composedestinations.annotation.Destination
 import dev.jdtech.jellyfin.api.JellyfinApi
 import dev.jdtech.jellyfin.models.CollectionType
+import dev.jdtech.jellyfin.models.FindroidItem
+import dev.jdtech.jellyfin.ui.dummy.dummyMovies
+import dev.jdtech.jellyfin.ui.theme.FindroidTheme
 import dev.jdtech.jellyfin.viewmodels.LibraryViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import java.util.UUID
 
-@OptIn(ExperimentalTvMaterial3Api::class)
 @Destination
 @Composable
 fun LibraryScreen(
@@ -42,7 +49,20 @@ fun LibraryScreen(
     val api = JellyfinApi.getInstance(context)
 
     val delegatedUiState by libraryViewModel.uiState.collectAsState()
-    when (val uiState = delegatedUiState) {
+
+    LibraryScreenLayout(
+        uiState = delegatedUiState,
+        baseUrl = api.api.baseUrl ?: ""
+    )
+}
+
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Composable
+private fun LibraryScreenLayout(
+    uiState: LibraryViewModel.UiState,
+    baseUrl: String,
+) {
+    when (uiState) {
         is LibraryViewModel.UiState.Loading -> Text(text = "LOADING")
         is LibraryViewModel.UiState.Normal -> {
             val items = uiState.items.collectAsLazyPagingItems()
@@ -63,7 +83,7 @@ fun LibraryScreen(
                             image = {
                                 ItemPoster(
                                     item = item,
-                                    baseUrl = api.api.baseUrl ?: "",
+                                    baseUrl = baseUrl,
                                     direction = Direction.VERTICAL,
                                 )
                             },
@@ -81,5 +101,20 @@ fun LibraryScreen(
             }
         }
         is LibraryViewModel.UiState.Error -> Text(text = uiState.error.toString())
+    }
+}
+
+@OptIn(ExperimentalTvMaterial3Api::class)
+@Preview(widthDp = 960, heightDp = 540)
+@Composable
+private fun LibraryScreenLayoutPreview() {
+    val data: Flow<PagingData<FindroidItem>> = flowOf(PagingData.from(dummyMovies))
+    FindroidTheme {
+        Surface {
+            LibraryScreenLayout(
+                uiState = LibraryViewModel.UiState.Normal(data),
+                baseUrl = "https://demo.jellyfin.org/stable"
+            )
+        }
     }
 }
