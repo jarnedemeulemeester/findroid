d2b536e50f7fbd80b03746fc363e8e531a7bcd9c
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-07-18 22:54:20 +0200

    fix: use "latest" + library name in home screen

--- app/tv/src/main/java/dev/jdtech/jellyfin/ui/HomeScreen.kt
+++ app/tv/src/main/java/dev/jdtech/jellyfin/ui/HomeScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -102,7 +103,7 @@ private fun HomeScreenLayout(
                         }
                         is HomeItem.ViewItem -> {
                             Text(
-                                text = homeItem.view.name,
+                                text = stringResource(id = CoreR.string.latest_library, homeItem.view.name),
                                 style = MaterialTheme.typography.headlineMedium,
                                 modifier = Modifier.padding(start = 32.dp),
                             )
