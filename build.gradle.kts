43d6d5ce1f048e4811ffefc776816e8b42c0f83d
jarnedemeulemeester <jarnedemeulemeester@gmail.com>
2023-09-01 14:39:00 +0200

    refactor: migrate from kapt to ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
     alias(libs.plugins.hilt) apply false
