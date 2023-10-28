1af8f89c1025484ee56fec66b379f074ad0ea84e
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-10-28 12:30:52 +0200

    refactor: remove `downloadsMigrated` preference

--- preferences/src/main/java/dev/jdtech/jellyfin/AppPreferences.kt
+++ preferences/src/main/java/dev/jdtech/jellyfin/AppPreferences.kt
@@ -144,11 +144,4 @@ constructor(
                 putString(Constants.PREF_SORT_ORDER, value)
             }
         }
-
-    // Temp
-    var downloadsMigrated
-        get() = sharedPreferences.getBoolean("downloadsMigrated", false)
-        set(value) = sharedPreferences.edit {
-            putBoolean("downloadsMigrated", value)
-        }
 }
