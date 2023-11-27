9afb6decc99d06eaf290a0596bfdc461227eac56
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-11-27 22:05:37 +0100

    ci: upload tv artifacts and don't build universal apks

--- app/phone/build.gradle.kts
+++ app/phone/build.gradle.kts
@@ -57,7 +57,6 @@ android {
             isEnable = true
             reset()
             include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-            isUniversalApk = true
         }
     }
 
--- app/tv/build.gradle.kts
+++ app/tv/build.gradle.kts
@@ -52,7 +52,6 @@ android {
             isEnable = true
             reset()
             include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-            isUniversalApk = true
         }
     }
 
