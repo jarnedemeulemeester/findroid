fd9325e2db04d7c6b8e56fbb6b09e5ba5354b5e0
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-12-03 14:07:34 +0100

    fix: workManagerConfiguration is now a property

--- app/phone/src/main/java/dev/jdtech/jellyfin/BaseApplication.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/BaseApplication.kt
@@ -22,8 +22,8 @@ class BaseApplication : Application(), Configuration.Provider, ImageLoaderFactor
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
 
