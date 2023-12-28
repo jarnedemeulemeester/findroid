77c2a77ca20aabf707c6fd175861e1543388dc18
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-12-28 17:21:32 +0100

    refactor(mpv): implement track selection via TrackSelectionParameters
    Need to add ability to disable track type

--- player/video/src/main/java/dev/jdtech/jellyfin/dialogs/TrackSelectionDialogFragment.kt
+++ player/video/src/main/java/dev/jdtech/jellyfin/dialogs/TrackSelectionDialogFragment.kt
@@ -3,13 +3,15 @@ package dev.jdtech.jellyfin.dialogs
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.media3.common.C
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import dev.jdtech.jellyfin.mpv.MPVPlayer
+import dev.jdtech.jellyfin.getTrackNames
 import dev.jdtech.jellyfin.mpv.TrackType
 import dev.jdtech.jellyfin.player.video.R
 import dev.jdtech.jellyfin.viewmodels.PlayerActivityViewModel
 import java.lang.IllegalStateException
 
+// TODO add option to disable track type
 class TrackSelectionDialogFragment(
     private val type: TrackType,
     private val viewModel: PlayerActivityViewModel,
@@ -19,14 +21,15 @@ class TrackSelectionDialogFragment(
             TrackType.AUDIO -> {
                 return activity?.let { activity ->
                     val builder = MaterialAlertDialogBuilder(activity)
+                    val tracksGroups = viewModel.player.currentTracks.groups.filter { it.type == C.TRACK_TYPE_AUDIO }
                     builder.setTitle(getString(R.string.select_audio_track))
                         .setSingleChoiceItems(
-                            getTrackNames(viewModel.currentAudioTracks),
-                            viewModel.currentAudioTracks.indexOfFirst { it.selected },
+                            tracksGroups.getTrackNames(),
+                            tracksGroups.indexOfFirst { it.isSelected },
                         ) { dialog, which ->
                             viewModel.switchToTrack(
                                 TrackType.AUDIO,
-                                viewModel.currentAudioTracks[which],
+                                tracksGroups[which],
                             )
                             dialog.dismiss()
                         }
@@ -36,14 +39,15 @@ class TrackSelectionDialogFragment(
             TrackType.SUBTITLE -> {
                 return activity?.let { activity ->
                     val builder = MaterialAlertDialogBuilder(activity)
+                    val tracksGroups = viewModel.player.currentTracks.groups.filter { it.type == C.TRACK_TYPE_TEXT }
                     builder.setTitle(getString(R.string.select_subtile_track))
                         .setSingleChoiceItems(
-                            getTrackNames(viewModel.currentSubtitleTracks),
-                            viewModel.currentSubtitleTracks.indexOfFirst { if (viewModel.disableSubtitle) it.ffIndex == -1 else it.selected },
+                            tracksGroups.getTrackNames(),
+                            tracksGroups.indexOfFirst { it.isSelected },
                         ) { dialog, which ->
                             viewModel.switchToTrack(
                                 TrackType.SUBTITLE,
-                                viewModel.currentSubtitleTracks[which],
+                                tracksGroups[which],
                             )
                             dialog.dismiss()
                         }
@@ -54,15 +58,5 @@ class TrackSelectionDialogFragment(
                 throw IllegalStateException("TrackType must be AUDIO or SUBTITLE")
             }
         }
-    }
-
-    private fun getTrackNames(tracks: List<MPVPlayer.Companion.Track>): Array<String> {
-        return tracks.map { track ->
-            val nameParts: MutableList<String> = mutableListOf()
-            if (track.title.isNotEmpty()) nameParts.add(track.title)
-            if (track.lang.isNotEmpty()) nameParts.add(track.lang)
-            if (track.codec.isNotEmpty()) nameParts.add(track.codec)
-            nameParts.joinToString(separator = " - ")
-        }.toTypedArray()
     }
 }
--- player/video/src/main/java/dev/jdtech/jellyfin/mpv/TrackType.kt
+++ player/video/src/main/java/dev/jdtech/jellyfin/mpv/TrackType.kt
@@ -1,7 +1,21 @@
 package dev.jdtech.jellyfin.mpv
 
+import androidx.media3.common.C
+
 enum class TrackType(val type: String) {
     VIDEO("video"),
     AUDIO("audio"),
     SUBTITLE("sub"),
+    ;
+
+    companion object {
+        fun fromMedia3TrackType(trackType: Int): TrackType {
+            return when (trackType) {
+                C.TRACK_TYPE_VIDEO -> VIDEO
+                C.TRACK_TYPE_AUDIO -> AUDIO
+                C.TRACK_TYPE_TEXT -> SUBTITLE
+                else -> SUBTITLE
+            }
+        }
+    }
 }
