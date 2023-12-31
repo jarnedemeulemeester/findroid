1c893d005940b99fb6466f2d9a8bb0da96ff8bcc
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-12-31 20:59:31 +0100

    refactor(tv): remove unused composables

--- app/tv/src/main/java/dev/jdtech/jellyfin/ui/HomeScreen.kt
+++ app/tv/src/main/java/dev/jdtech/jellyfin/ui/HomeScreen.kt
@@ -1,12 +1,9 @@
 package dev.jdtech.jellyfin.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -16,14 +13,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.tv.foundation.lazy.list.TvLazyColumn
 import androidx.tv.foundation.lazy.list.TvLazyRow
@@ -186,23 +180,6 @@ private fun HomeScreenLayoutPreview() {
             uiState = HomeViewModel.UiState.Normal(dummyHomeItems),
             isLoading = {},
             onClick = {},
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Header(modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .height(80.dp),
-    ) {
-        Image(
-            painter = painterResource(id = CoreR.drawable.ic_banner),
-            contentDescription = null,
-            modifier = Modifier.height(40.dp),
         )
     }
 }
