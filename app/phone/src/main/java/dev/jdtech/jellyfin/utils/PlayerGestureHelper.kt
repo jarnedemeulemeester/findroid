22463b04183f334cc483912f11066ffe6ec3d89c
cd16b <davidecavagnola@gmail.com>
2024-02-02 13:04:06 +0100

    val enableZoom for readability

--- app/phone/src/main/java/dev/jdtech/jellyfin/utils/PlayerGestureHelper.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/utils/PlayerGestureHelper.kt
@@ -330,8 +330,8 @@ class PlayerGestureHelper(
                 lastScaleEvent = SystemClock.elapsedRealtime()
                 val scaleFactor = detector.scaleFactor
                 if (abs(scaleFactor - Constants.ZOOM_SCALE_BASE) > Constants.ZOOM_SCALE_THRESHOLD) {
-                    isZoomEnabled = scaleFactor > 1
-                    updateZoomMode(isZoomEnabled)
+                     val enableZoom= scaleFactor > 1
+                    updateZoomMode(enableZoom)
                 }
                 return true
             }
