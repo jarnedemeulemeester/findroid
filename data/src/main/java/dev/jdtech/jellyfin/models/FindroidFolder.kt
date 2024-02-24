23d000f8c0f5994b931fe3f661eeeccffc235ce7
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-02-24 17:22:46 +0100

    refactor: add default values to `FindroidFolder`

--- data/src/main/java/dev/jdtech/jellyfin/models/FindroidFolder.kt
+++ data/src/main/java/dev/jdtech/jellyfin/models/FindroidFolder.kt
@@ -2,19 +2,18 @@ package dev.jdtech.jellyfin.models
 
 import dev.jdtech.jellyfin.repository.JellyfinRepository
 import org.jellyfin.sdk.model.api.BaseItemDto
-import org.jellyfin.sdk.model.api.PlayAccess
 import java.util.UUID
 
 data class FindroidFolder(
     override val id: UUID,
     override val name: String,
-    override val originalTitle: String?,
-    override val overview: String,
-    override val sources: List<FindroidSource>,
+    override val originalTitle: String? = null,
+    override val overview: String = "",
     override val played: Boolean,
     override val favorite: Boolean,
-    override val canPlay: Boolean,
-    override val canDownload: Boolean,
+    override val canPlay: Boolean = false,
+    override val canDownload: Boolean = false,
+    override val sources: List<FindroidSource> = emptyList(),
     override val runtimeTicks: Long = 0L,
     override val playbackPositionTicks: Long = 0L,
     override val unplayedItemCount: Int?,
@@ -27,14 +26,9 @@ fun BaseItemDto.toFindroidFolder(
     return FindroidFolder(
         id = id,
         name = name.orEmpty(),
-        originalTitle = originalTitle,
-        overview = overview.orEmpty(),
         played = userData?.played ?: false,
         favorite = userData?.isFavorite ?: false,
-        canPlay = playAccess != PlayAccess.NONE,
-        canDownload = canDownload ?: false,
         unplayedItemCount = userData?.unplayedItemCount,
-        sources = emptyList(),
         images = toFindroidImages(jellyfinRepository),
     )
 }
