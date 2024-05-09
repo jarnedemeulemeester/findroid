0fa3093d0c5c2df356a5808b877a242e1d7e65f1
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-05-09 22:05:52 +0200

    fix: enable core library desugaring

--- app/tv/build.gradle.kts
+++ app/tv/build.gradle.kts
@@ -56,6 +56,8 @@ android {
     }
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
         sourceCompatibility = Versions.java
         targetCompatibility = Versions.java
     }
@@ -110,6 +112,8 @@ dependencies {
     ksp(libs.hilt.compiler)
     implementation(libs.jellyfin.core)
     implementation(libs.media3.ffmpeg.decoder)
+
+    coreLibraryDesugaring(libs.android.desugar.jdk)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
