03ae0c0fb44ff90eac922564e52107a0e6338b48
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-08-04 21:17:28 +0200

    build: bump versionCode to 26 and versionName to 0.15.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val appCode = 25
-    const val appName = "0.14.2"
+    const val appCode = 26
+    const val appName = "0.15.0"
 
     const val compileSdk = 35
     const val buildTools = "35.0.0"
