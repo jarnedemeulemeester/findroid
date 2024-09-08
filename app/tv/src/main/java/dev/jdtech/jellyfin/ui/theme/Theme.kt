f0b0c0a5fa296f399492505a9a6ab2fcc99ddc0a
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-09-08 17:34:45 +0200

    fix(tv): replace `Surface` with `CompositionLocalProvider`
    Fix no video output

--- app/tv/src/main/java/dev/jdtech/jellyfin/ui/theme/Theme.kt
+++ app/tv/src/main/java/dev/jdtech/jellyfin/ui/theme/Theme.kt
@@ -3,15 +3,16 @@ package dev.jdtech.jellyfin.ui.theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.tv.material3.Surface
-import androidx.tv.material3.SurfaceDefaults
+import androidx.tv.material3.contentColorFor as contentColorForTv
+import androidx.tv.material3.LocalContentColor as LocalContentColorTv
 import androidx.tv.material3.MaterialTheme as MaterialThemeTv
 
 @Composable
@@ -23,34 +24,29 @@ fun FindroidTheme(
         typography = Typography,
         shapes = shapes,
     ) {
-        CompositionLocalProvider(
-            LocalSpacings provides Spacings(),
+        MaterialThemeTv(
+            colorScheme = ColorSchemeTv,
+            typography = TypographyTv,
+            shapes = shapesTv
         ) {
-            MaterialThemeTv(
-                colorScheme = ColorSchemeTv,
-                typography = TypographyTv,
-                shapes = shapesTv,
-                content = {
-                    Surface(
-                        colors = SurfaceDefaults.colors(
-                            containerColor = androidx.tv.material3.MaterialTheme.colorScheme.background,
-                        ),
-                        shape = RectangleShape,
-                    ) {
-                        Box(
-                            modifier = Modifier.background(
-                                Brush.linearGradient(
-                                    listOf(
-                                        Color.Black,
-                                        Color(0xFF001721),
-                                    ),
-                                ),
+            CompositionLocalProvider(
+                LocalContentColor provides contentColorFor(MaterialTheme.colorScheme.background),
+                LocalContentColorTv provides contentColorForTv(MaterialThemeTv.colorScheme.background),
+                LocalSpacings provides Spacings(),
+            ) {
+                Box(
+                    modifier = Modifier.background(
+                        Brush.linearGradient(
+                            listOf(
+                                Color.Black,
+                                Color(0xFF001721),
                             ),
-                            content = content,
-                        )
-                    }
-                },
-            )
+                        ),
+                    )
+                ) {
+                    content()
+                }
+            }
         }
     }
 }
