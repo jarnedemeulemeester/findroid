7c8794032c3d77ae809792fbd8e92a072a9ec689
david <davidecavagnola@gmail.com>
2024-11-02 20:53:36 +0100

    FindroidSegmentType, preferences summary

--- data/src/main/java/dev/jdtech/jellyfin/models/FindroidSegment.kt
+++ data/src/main/java/dev/jdtech/jellyfin/models/FindroidSegment.kt
@@ -3,8 +3,21 @@ package dev.jdtech.jellyfin.models
 import org.jellyfin.sdk.model.api.MediaSegmentDto
 import org.jellyfin.sdk.model.api.MediaSegmentType
 
+enum class FindroidSegmentType {
+    INTRO, OUTRO, RECAP, PREVIEW, COMMERCIAL, UNKNOWN
+}
+
+private fun MediaSegmentType.toFindroidSegmentType(): FindroidSegmentType = when (this) {
+    MediaSegmentType.UNKNOWN -> FindroidSegmentType.UNKNOWN
+    MediaSegmentType.INTRO -> FindroidSegmentType.INTRO
+    MediaSegmentType.OUTRO -> FindroidSegmentType.OUTRO
+    MediaSegmentType.RECAP -> FindroidSegmentType.RECAP
+    MediaSegmentType.PREVIEW -> FindroidSegmentType.PREVIEW
+    MediaSegmentType.COMMERCIAL -> FindroidSegmentType.COMMERCIAL
+}
+
 data class FindroidSegment(
-    var type: MediaSegmentType = MediaSegmentType.UNKNOWN,
+    var type: FindroidSegmentType = FindroidSegmentType.UNKNOWN,
     val startTicks: Long,
     val endTicks: Long,
 )
@@ -19,7 +32,7 @@ fun FindroidSegmentDto.toFindroidSegment(): FindroidSegment {
 
 fun MediaSegmentDto.toFindroidSegment(): FindroidSegment {
     return FindroidSegment(
-        type = type,
+        type = type.toFindroidSegmentType(),
         startTicks = startTicks / 10000,
         endTicks = endTicks / 10000,
     )
