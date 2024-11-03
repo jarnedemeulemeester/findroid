dd9e5f8adff46deb667715a0bf4c037332617456
Gang <freegang555@gmail.com>
2024-11-03 16:44:21 +0800

    feat: add random options to sorting

--- data/src/main/java/dev/jdtech/jellyfin/models/SortBy.kt
+++ data/src/main/java/dev/jdtech/jellyfin/models/SortBy.kt
@@ -2,6 +2,7 @@ package dev.jdtech.jellyfin.models
 
 enum class SortBy(val sortString: String) {
     NAME("SortName"),
+    Random("Random"),
     IMDB_RATING("CommunityRating"),
     PARENTAL_RATING("CriticRating"),
     DATE_ADDED("DateCreated"),
