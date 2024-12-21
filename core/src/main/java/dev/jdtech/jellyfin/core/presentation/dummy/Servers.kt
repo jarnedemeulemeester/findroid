df206cefb4293225f9bb15b00b73a2f8d051b7e9
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-12-21 18:00:10 +0100

    refactor: copy dummy items to core module

---
+++ core/src/main/java/dev/jdtech/jellyfin/core/presentation/dummy/Servers.kt
@@ -0,0 +1,22 @@
+package dev.jdtech.jellyfin.core.presentation.dummy
+
+import dev.jdtech.jellyfin.models.DiscoveredServer
+import dev.jdtech.jellyfin.models.Server
+import java.util.UUID
+
+val dummyDiscoveredServer = DiscoveredServer(
+    id = "",
+    name = "Demo server",
+    address = "https://demo.jellyfin.org/stable",
+)
+
+val dummyDiscoveredServers = listOf(dummyDiscoveredServer)
+
+val dummyServer = Server(
+    id = "",
+    name = "Demo server",
+    currentServerAddressId = UUID.randomUUID(),
+    currentUserId = UUID.randomUUID(),
+)
+
+val dummyServers = listOf(dummyServer)
---
+++ core/src/main/java/dev/jdtech/jellyfin/core/presentation/dummy/Show.kt
@@ -0,0 +1,30 @@
+package dev.jdtech.jellyfin.core.presentation.dummy
+
+import dev.jdtech.jellyfin.models.FindroidImages
+import dev.jdtech.jellyfin.models.FindroidShow
+import java.time.LocalDateTime
+import java.util.UUID
+
+val dummyShow = FindroidShow(
+    id = UUID.randomUUID(),
+    name = "Attack on Titan",
+    originalTitle = null,
+    overview = "After his hometown is destroyed and his mother is killed, young Eren Yeager vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.",
+    sources = emptyList(),
+    played = false,
+    favorite = false,
+    canPlay = true,
+    canDownload = false,
+    runtimeTicks = 0L,
+    communityRating = 8.8f,
+    endDate = LocalDateTime.parse("2023-11-04T00:00:00"),
+    genres = listOf("Action", "Sience Fiction", "Adventure"),
+    images = FindroidImages(),
+    officialRating = "TV-MA",
+    people = emptyList(),
+    productionYear = 2013,
+    seasons = emptyList(),
+    status = "Ended",
+    trailer = null,
+    unplayedItemCount = 20,
+)
