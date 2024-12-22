4055b5f2866121dddee495163202315418bb8aff
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-12-22 11:52:58 +0100

    feat(home): add loading indicator

--- app/phone/src/main/java/dev/jdtech/jellyfin/presentation/film/HomeScreen.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/presentation/film/HomeScreen.kt
@@ -14,8 +14,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
@@ -100,6 +102,13 @@ private fun HomeScreenLayout(
                             painter = painterResource(CoreR.drawable.ic_search),
                             contentDescription = null
                         )
+                    },
+                    trailingIcon = {
+                        if (state.isLoading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(32.dp)
+                            )
+                        }
                     }
                 )
             },
