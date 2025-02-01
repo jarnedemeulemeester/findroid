a1126e71ecf3b7fe918e76c8b643512806518d60
jarnedemeulemeester <jarnedemeulemeester@gmail.com>
2025-02-01 15:23:38 +0100

    feat(tv): add title to group

--- app/tv/src/main/java/dev/jdtech/jellyfin/presentation/settings/components/SettingsGroupCard.kt
+++ app/tv/src/main/java/dev/jdtech/jellyfin/presentation/settings/components/SettingsGroupCard.kt
@@ -1,7 +1,9 @@
 package dev.jdtech.jellyfin.presentation.settings.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -9,11 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.util.fastForEachIndexed
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Surface
 import androidx.tv.material3.SurfaceDefaults
+import androidx.tv.material3.Text
 import dev.jdtech.jellyfin.Constants
 import dev.jdtech.jellyfin.models.Preference
 import dev.jdtech.jellyfin.models.PreferenceCategory
@@ -32,56 +36,66 @@ fun SettingsGroupCard(
     modifier: Modifier = Modifier,
     onFocusChange: (FocusState, Preference) -> Unit = { _, _ -> },
 ) {
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        colors = SurfaceDefaults.colors(
-            containerColor = MaterialTheme.colorScheme.surface,
-        ),
-        modifier = modifier
-            .fillMaxWidth(),
-    ) {
-        Column {
-            group.preferences.fastForEachIndexed { index, preference ->
-                when (preference) {
-                    is PreferenceCategory -> SettingsCategoryCard(
-                        preference = preference,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .onFocusChanged {
-                                onFocusChange(it, preference)
+    Column {
+        group.nameStringResource?.let {
+            Text(
+                text = stringResource(it),
+                modifier = Modifier.padding(start = MaterialTheme.spacings.medium),
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier.height(MaterialTheme.spacings.small))
+        }
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            colors = SurfaceDefaults.colors(
+                containerColor = MaterialTheme.colorScheme.surface,
+            ),
+            modifier = modifier
+                .fillMaxWidth(),
+        ) {
+            Column {
+                group.preferences.fastForEachIndexed { index, preference ->
+                    when (preference) {
+                        is PreferenceCategory -> SettingsCategoryCard(
+                            preference = preference,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .onFocusChanged {
+                                    onFocusChange(it, preference)
+                                },
+                        )
+                        is PreferenceSwitch -> SettingsSwitchCard(
+                            preference = preference,
+                            onClick = {
+                                onAction(
+                                    SettingsAction.OnUpdate(
+                                        preference.copy(value = !preference.value),
+                                    ),
+                                )
                             },
-                    )
-                    is PreferenceSwitch -> SettingsSwitchCard(
-                        preference = preference,
-                        onClick = {
-                            onAction(
-                                SettingsAction.OnUpdate(
-                                    preference.copy(value = !preference.value),
-                                ),
-                            )
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .onFocusChanged {
-                                onFocusChange(it, preference)
-                            },
-                    )
-                    is PreferenceSelect -> SettingsSelectCard(
-                        preference = preference,
-                        onClick = { },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .onFocusChanged {
-                                onFocusChange(it, preference)
-                            },
-                    )
-                }
-                if (index < group.preferences.lastIndex) {
-                    HorizontalDivider(
-                        modifier = Modifier
-                            .padding(horizontal = MaterialTheme.spacings.medium),
-                        color = DividerDefaults.color.copy(alpha = 0.4f)
-                    )
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .onFocusChanged {
+                                    onFocusChange(it, preference)
+                                },
+                        )
+                        is PreferenceSelect -> SettingsSelectCard(
+                            preference = preference,
+                            onClick = { },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .onFocusChanged {
+                                    onFocusChange(it, preference)
+                                },
+                        )
+                    }
+                    if (index < group.preferences.lastIndex) {
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .padding(horizontal = MaterialTheme.spacings.medium),
+                            color = DividerDefaults.color.copy(alpha = 0.4f)
+                        )
+                    }
                 }
             }
         }
