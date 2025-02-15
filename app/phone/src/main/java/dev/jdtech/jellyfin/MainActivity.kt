3ae7131d4e7692c08575ac4adfb82761cb3ef545
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-02-15 10:50:14 +0100

    fix(phone): fix theme selection on android < 12

--- app/phone/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/MainActivity.kt
@@ -1,10 +1,10 @@
 package dev.jdtech.jellyfin
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
@@ -19,7 +19,7 @@ import dev.jdtech.jellyfin.viewmodels.MainViewModel
 import dev.jdtech.jellyfin.work.SyncWorker
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
