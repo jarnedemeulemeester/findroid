5235afb33cb2ffd6e4fea0b4f5d9485758c68738
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-03-02 12:03:31 +0100

    refactor: set segmented button inactive container color to transparent

--- app/phone/src/main/java/dev/jdtech/jellyfin/presentation/film/components/SortByDialog.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/presentation/film/components/SortByDialog.kt
@@ -33,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
@@ -114,7 +115,7 @@ fun SortByDialog(
                                 count = orderOptions.size,
                             ),
                             colors = SegmentedButtonDefaults.colors(
-                                inactiveContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                                inactiveContainerColor = Color.Transparent,
                             ),
                             icon = {
                                 AnimatedVisibility(
