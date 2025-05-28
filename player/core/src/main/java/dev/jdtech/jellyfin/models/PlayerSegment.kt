c5028c5a010e60b72a83c6a7708490a4fba1221e
Davide <davidecavagnola@gmail.com>
2025-05-28 20:06:12 +0200

    refactor: Rename skip credits string and make PlayerSegment a data class
    The string `player_controls_skip_credits` has been renamed to `player_controls_skip_outro` for clarity.
    
    The `PlayerSegment` class has been converted to a data class. This change is primarily for code style consistency and does not affect functionality.

--- player/core/src/main/java/dev/jdtech/jellyfin/models/PlayerSegment.kt
+++ player/core/src/main/java/dev/jdtech/jellyfin/models/PlayerSegment.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class PlayerSegment(
+data class PlayerSegment(
     val type: FindroidSegmentType,
     val startTicks: Long,
     val endTicks: Long,
