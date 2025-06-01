88e021a47263d0f2798cdea502f83dedc61b6d4c
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-06-01 11:42:21 +0200

    fix: set background color for item poster

--- app/phone/src/main/java/dev/jdtech/jellyfin/presentation/film/components/ItemPoster.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/presentation/film/components/ItemPoster.kt
@@ -1,11 +1,11 @@
 package dev.jdtech.jellyfin.presentation.film.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import coil3.compose.AsyncImage
 import dev.jdtech.jellyfin.models.FindroidEpisode
@@ -37,11 +37,13 @@ fun ItemPoster(
 
     AsyncImage(
         model = imageUri,
-        placeholder = ColorPainter(MaterialTheme.colorScheme.surfaceContainer),
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = modifier
             .fillMaxWidth()
-            .aspectRatio(if (direction == Direction.HORIZONTAL) 1.77f else 0.66f),
+            .aspectRatio(if (direction == Direction.HORIZONTAL) 1.77f else 0.66f)
+            .background(
+                MaterialTheme.colorScheme.surfaceContainer,
+            ),
     )
 }
