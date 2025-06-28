135e58577d31bbb1d6d47c9782d78f6699925859
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-06-28 10:35:19 +0200

    lint: run ktlint

--- player/video/src/main/java/dev/jdtech/jellyfin/Extensions.kt
+++ player/video/src/main/java/dev/jdtech/jellyfin/Extensions.kt
@@ -10,13 +10,16 @@ fun List<Tracks.Group>.getTrackNames(): Array<String> {
         val format = group.mediaTrackGroup.getFormat(0)
         nameParts.run {
             add(format.label)
-            add(format.language?.let { if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
-                Locale.of(it.split("-").last()).displayLanguage
-            } else {
-                @Suppress("DEPRECATION")
-                Locale(it.split("-").last()).displayLanguage
-            }
-            })
+            add(
+                format.language?.let {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
+                        Locale.of(it.split("-").last()).displayLanguage
+                    } else {
+                        @Suppress("DEPRECATION")
+                        Locale(it.split("-").last()).displayLanguage
+                    }
+                },
+            )
             add(format.codecs)
             filterNotNull().joinToString(separator = " - ")
         }
