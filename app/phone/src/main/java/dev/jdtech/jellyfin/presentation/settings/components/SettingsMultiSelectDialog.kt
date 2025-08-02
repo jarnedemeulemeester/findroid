906706af80955214e48b9d9de08f426c1f26314e
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-08-02 23:48:44 +0200

    feat(phone): use BaseDialog for SettingsMultiSelectDialog

--- app/phone/src/main/java/dev/jdtech/jellyfin/presentation/settings/components/SettingsMultiSelectDialog.kt
+++ app/phone/src/main/java/dev/jdtech/jellyfin/presentation/settings/components/SettingsMultiSelectDialog.kt
@@ -1,20 +1,13 @@
 package dev.jdtech.jellyfin.presentation.settings.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -30,8 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
+import dev.jdtech.jellyfin.presentation.components.BaseDialog
 import dev.jdtech.jellyfin.presentation.theme.FindroidTheme
 import dev.jdtech.jellyfin.presentation.theme.spacings
 import dev.jdtech.jellyfin.settings.domain.models.Preference
@@ -56,60 +48,47 @@ fun SettingsMultiSelectDialog(
         mutableStateOf(preference.value?.toSet() ?: emptySet())
     }
 
-    Dialog(
-        onDismissRequest = onDismissRequest,
-    ) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(max = 540.dp),
-            shape = RoundedCornerShape(28.dp),
-        ) {
-            Column(
-                modifier = Modifier.padding(MaterialTheme.spacings.default),
+    BaseDialog(
+        title = stringResource(preference.nameStringResource),
+        onDismiss = onDismissRequest,
+        negativeButton = {
+            TextButton(
+                onClick = onDismissRequest,
             ) {
                 Text(
-                    text = stringResource(preference.nameStringResource),
-                    style = MaterialTheme.typography.headlineSmall,
+                    text = stringResource(SettingsR.string.cancel),
                 )
-                Spacer(modifier = Modifier.height(MaterialTheme.spacings.medium))
-                if (!isAtTop) {
-                    HorizontalDivider()
-                }
-                LazyColumn(
-                    modifier = Modifier.fillMaxWidth(),
-                    state = lazyListState,
-                ) {
-                    items(options) { option ->
-                        SettingsMultiSelectDialogItem(
-                            option = option,
-                            checked = selectedOptions.contains(option.first),
-                            onCheckedChange = { key ->
-                                selectedOptions = if (selectedOptions.contains(key)) {
-                                    selectedOptions - setOfNotNull(key)
-                                } else {
-                                    selectedOptions + listOfNotNull(key)
-                                }
-                            },
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(MaterialTheme.spacings.default))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    TextButton(
-                        onClick = onDismissRequest,
-                    ) {
-                        Text(text = stringResource(SettingsR.string.cancel))
-                    }
-                    TextButton(
-                        onClick = { onUpdate(selectedOptions.ifEmpty { emptySet() }) },
-                    ) {
-                        Text(text = stringResource(SettingsR.string.save))
-                    }
-                }
+            }
+        },
+        positiveButton = {
+            TextButton(
+                onClick = { onUpdate(selectedOptions.ifEmpty { emptySet() }) },
+            ) {
+                Text(
+                    text = stringResource(SettingsR.string.save),
+                )
+            }
+        },
+    ) {
+        if (!isAtTop) {
+            HorizontalDivider()
+        }
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            state = lazyListState,
+        ) {
+            items(options) { option ->
+                SettingsMultiSelectDialogItem(
+                    option = option,
+                    checked = selectedOptions.contains(option.first),
+                    onCheckedChange = { key ->
+                        selectedOptions = if (selectedOptions.contains(key)) {
+                            selectedOptions - setOfNotNull(key)
+                        } else {
+                            selectedOptions + listOfNotNull(key)
+                        }
+                    },
+                )
             }
         }
     }
