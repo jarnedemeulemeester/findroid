f0347f164be592efc79d987daba9e1bb37b9004c
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2025-12-28 18:26:40 +0100

    fix(search): cancel previous search job when starting new one

--- modes/film/src/main/java/dev/jdtech/jellyfin/film/presentation/search/SearchViewModel.kt
+++ modes/film/src/main/java/dev/jdtech/jellyfin/film/presentation/search/SearchViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.jdtech.jellyfin.repository.JellyfinRepository
 import javax.inject.Inject
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -16,23 +18,27 @@ class SearchViewModel @Inject constructor(private val repository: JellyfinReposi
     private val _state = MutableStateFlow(SearchState())
     val state = _state.asStateFlow()
 
+    var currentJob: Job? = null
+
     private fun search(query: String) {
-        viewModelScope.launch {
-            if (query.isBlank()) {
-                _state.emit(SearchState(items = emptyList(), loading = false))
-                return@launch
-            }
+        currentJob?.cancel()
+        currentJob =
+            viewModelScope.launch {
+                try {
+                    if (query.isBlank()) {
+                        _state.emit(SearchState(items = emptyList(), loading = false))
+                        return@launch
+                    }
 
-            _state.emit(_state.value.copy(loading = true))
-            try {
-                val items = repository.getSearchItems(query)
+                    _state.emit(_state.value.copy(loading = true))
+                    val items = repository.getSearchItems(query)
 
-                _state.emit(SearchState(items = items, loading = false))
-            } catch (e: Exception) {
-                Timber.e(e)
-                _state.emit(_state.value.copy(loading = false))
+                    _state.emit(SearchState(items = items, loading = false))
+                } catch (_: CancellationException) {} catch (e: Exception) {
+                    Timber.e(e)
+                    _state.emit(_state.value.copy(loading = false))
+                }
             }
-        }
     }
 
     fun onAction(action: SearchAction) {
