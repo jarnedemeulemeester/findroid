8e98253e61cca860f9eb3a644429104cc357615f
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2026-05-10 16:36:33 +0200

    chore: target sdk 37

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,10 +4,10 @@ object Versions {
     const val APP_CODE = 32
     const val APP_NAME = "1.0.2"
 
-    const val COMPILE_SDK = 36
-    const val TARGET_SDK = 36
+    const val COMPILE_SDK = 37
+    const val TARGET_SDK = 37
     const val MIN_SDK = 28
-    const val BUILD_TOOLS = "36.1.0"
+    const val BUILD_TOOLS = "37.0.0"
 
     val JAVA = JavaVersion.VERSION_21
 }
