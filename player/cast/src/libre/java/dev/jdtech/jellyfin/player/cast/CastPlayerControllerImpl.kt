bd6a28656fd4c66fdc2a02090c846f5f95650c17
PickleRick <davidecavagnola@gmail.com>
2026-06-29 12:41:07 +0200

    refactor(cast): clean up libre implementation

--- player/cast/src/libre/java/dev/jdtech/jellyfin/player/cast/CastPlayerControllerImpl.kt
+++ player/cast/src/libre/java/dev/jdtech/jellyfin/player/cast/CastPlayerControllerImpl.kt
@@ -1,28 +1,22 @@
 package dev.jdtech.jellyfin.player.cast
 
-import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
+import dev.jdtech.jellyfin.player.cast.models.CastPlayerState
 import dev.jdtech.jellyfin.player.core.domain.models.PlayerItem
 import dev.jdtech.jellyfin.player.core.domain.models.Track
-import dev.jdtech.jellyfin.player.cast.models.CastPlayerState
-import org.jellyfin.sdk.model.UUID
-import org.jellyfin.sdk.model.api.PlaybackInfoResponse
-import javax.inject.Inject
-import javax.inject.Singleton
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import org.jellyfin.sdk.model.UUID
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @Singleton
-class CastPlayerControllerImpl @Inject constructor(
-    @param:ApplicationContext private val context: Context
-) : CastPlayerController {
-
+class CastPlayerControllerImpl @Inject constructor() : CastPlayerController {
     private val _currentItem = MutableStateFlow<PlayerItem?>(null)
     override val currentItem: StateFlow<PlayerItem?> = _currentItem.asStateFlow()
 
-    private val _playbackState = MutableStateFlow(CastPlayerState())
-    override val playbackState: StateFlow<CastPlayerState> = _playbackState.asStateFlow()
+    private val _playerState = MutableStateFlow(CastPlayerState())
+    override val playerState: StateFlow<CastPlayerState> = _playerState.asStateFlow()
 
     private val _subtitleTracks = MutableStateFlow<List<Track>>(emptyList())
     override val subtitleTracks: StateFlow<List<Track>> = _subtitleTracks.asStateFlow()
@@ -30,18 +24,13 @@ class CastPlayerControllerImpl @Inject constructor(
     private val _audioTracks = MutableStateFlow<List<Track>>(emptyList())
     override val audioTracks: StateFlow<List<Track>> = _audioTracks.asStateFlow()
 
-    private val _volume = MutableStateFlow(1f)
-    override val volume: StateFlow<Float> = _volume.asStateFlow()
-
     override fun playItem(itemId: UUID, itemKind: String, startFromBeginning: Boolean) {}
     override fun play() {}
     override fun pause() {}
     override fun seekTo(position: Long) {}
     override fun seekToNext() {}
     override fun seekToPrevious() {}
-    override fun setVolume(volume: Float) {
-        _volume.value = volume
-    }
+    override fun setVolume(volume: Float) {}
     override fun setAudioTrack(track: Track?, itemId: UUID?) {}
     override fun setSubtitleTrack(track: Track?) {}
     override fun stop() {}
--- player/cast/src/libre/java/dev/jdtech/jellyfin/player/cast/CastSessionManagerImpl.kt
+++ player/cast/src/libre/java/dev/jdtech/jellyfin/player/cast/CastSessionManagerImpl.kt
@@ -1,19 +1,15 @@
 package dev.jdtech.jellyfin.player.cast
 
-import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.jdtech.jellyfin.player.cast.models.CastConnectionState
 import dev.jdtech.jellyfin.player.cast.models.Device
-import javax.inject.Inject
-import javax.inject.Singleton
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @Singleton
-class CastSessionManagerImpl @Inject constructor(
-    @param:ApplicationContext private val context: Context
-) : CastSessionManager {
+class CastSessionManagerImpl @Inject constructor() : CastSessionManager {
     override val isSupported = false
 
     private val _connectionState = MutableStateFlow(CastConnectionState.DISCONNECTED)
@@ -25,11 +21,9 @@ class CastSessionManagerImpl @Inject constructor(
     private val _connectedDevice = MutableStateFlow<Device?>(null)
     override val connectedDevice: StateFlow<Device?> = _connectedDevice.asStateFlow()
 
-    override val currentSession = MutableStateFlow<Any?>(null)
-    override val remoteMediaClient = MutableStateFlow<Any?>(null)
-
     override fun init() {}
     override fun updateDiscovery(flags: Int) {}
     override fun connect(device: Device) {}
     override fun disconnect() {}
+    override fun release() {}
 }
